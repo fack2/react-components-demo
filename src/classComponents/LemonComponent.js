f2bc93ba28a1f1282ffa954868d8a5672b6deed3
eadehemingway <eade.hemingway@gmail.com>
2019-08-18 11:39:41 +0300

    add method to lemon component

--- src/classComponents/LemonComponent.js
+++ src/classComponents/LemonComponent.js
@@ -4,19 +4,25 @@ import React from 'react'
 export class LemonComponent extends React.Component{
 
     state = {
-        word: "lemon"
+        word: "lemon", 
+        clicked: false
+    }
+
+    toggleClickedVal = () => {
+        const { clicked} = this.state
+        this.setState({clicked: !clicked})
     }
 
     render (){
-        const {word} = this.state
+        const {word, clicked} = this.state
         const {colour} = this.props
         return(
             <div className="lemon-component" style={{background: colour}}>
-                < button onClick={()=> this.setState({word: `${word}n`})}> click me </button>
-                <p> {word}</p>
+                < button onClick={()=> this.setState({word: `${word}n`})}> ADD NNNN </button>
+                <p > {word}</p>
+                <button onClick={this.toggleClickedVal }>change clicked val</button>
+                {clicked && (<p> CLICKED IS TRUE</p>)}
             </div>  
-
-
         )
     }
 
